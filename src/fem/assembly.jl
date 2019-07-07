d49f32c2f1d768cf1ab679abbb6943c0e0d38fb8
Zhengyu-Huang <hzyhzy2014@gmail.com>
2019-07-06 23:34:36 -0700

    add old implementation of force and stiff and comments

--- src/fem/assembly.jl
+++ src/fem/assembly.jl
@@ -89,6 +89,45 @@ function assembleStiffAndForce(globdat::GlobalData, domain::Domain)
     return Fint, K
 end
 
+# function assembleStiffAndForce(globdat::GlobalData, domain::Domain)
+#   Fint = zeros(Float64, domain.neqs)
+#   K = zeros(Float64, domain.neqs, domain.neqs)
+#   neles = domain.neles
+
+#   # Loop over the elements in the elementGroup
+#   for iele  = 1:neles
+#     element = domain.elements[iele]
+
+#     # Get the element nodes
+#     el_nodes = getNodes(element)
+
+#     # Get the element nodes
+#     el_eqns = getEqns(domain,iele)
+
+#     el_dofs = getDofs(domain,iele)
+
+#     #@show "iele", iele, el_dofs 
+    
+#     #@show "domain.state", iele, domain.state 
+
+#     el_state  = getState(domain,el_dofs)
+
+#     el_Dstate = getDstate(domain,el_dofs)
+#     # #@show "+++++", el_state, el_Dstate
+
+#     # Get the element contribution by calling the specified action
+#     #@info "ele id is ", iele
+#     fint, stiff  = getStiffAndForce(element, el_state, el_Dstate)
+
+#     # Assemble in the global array
+#     el_eqns_active = el_eqns .>= 1
+#     K[el_eqns[el_eqns_active], el_eqns[el_eqns_active]] += stiff[el_eqns_active,el_eqns_active]
+#     Fint[el_eqns[el_eqns_active]] += fint[el_eqns_active]
+#   end
+#   return Fint, sparse(K)
+# end
+
+
 function assembleMassMatrix!(globaldat::GlobalData, domain::Domain)
     Mlumped = zeros(Float64, domain.neqs)
     M = zeros(Float64, domain.neqs, domain.neqs)
