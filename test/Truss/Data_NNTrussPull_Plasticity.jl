fca860acc291bfdce02bac0c49ea32868de26db3
Zhengyu-Huang <hzyhzy2014@gmail.com>
2019-07-23 15:42:05 -0700

    cleaning 1d example

--- test/Truss/Data_NNTrussPull_Plasticity.jl
+++ test/Truss/Data_NNTrussPull_Plasticity.jl
@@ -8,29 +8,22 @@ using ADCME
 using LinearAlgebra
 
 include("NNTrussPull_Domain.jl")
-#testtype = "Plasticity1D" 
-testtype = "Elasticity1D" 
+testtype = "Plasticity1D" 
+#testtype = "Elasticity1D" 
 
 
 
 prop = Dict("name"=> testtype, "rho"=> 0.1, "E"=> 200, "B"=> 10.0,
             "sigmaY"=>0.300, "K"=>1/9*200, "A0"=> 1.0, "eta"=> 10.0)
-
-
 elements = []
 for i = 1:nx 
     elnodes = [i, i+1]; coords = nodes[elnodes,:];
     push!(elements, FiniteStrainTruss(coords,elnodes, prop, ngp))
-
 end
-
-
-
 domain = Domain(nodes, elements, ndofs, EBC, g, FBC, fext)
 state = zeros(domain.neqs)
 ∂u = zeros(domain.neqs)
 globdat = GlobalData(state,zeros(domain.neqs), zeros(domain.neqs),∂u, domain.neqs, gt, ft)
-
 assembleMassMatrix!(globdat, domain)
 # need to update state in domain from globdat
 updateStates!(domain, globdat)
--- test/Truss/Test_NNTrussPull_Plasticity.jl
+++ test/Truss/Test_NNTrussPull_Plasticity.jl
@@ -12,64 +12,24 @@ include("nnutil.jl")
 
 # testtype = "PlaneStressPlasticity"
 testtype = "NeuralNetwork2D"
-include("NNPlatePull_Domain.jl")
+include("NNTrussPull_Domain.jl")
 
 
-# np = pyimport("numpy")
-# nx, ny =  1,2
-# nnodes, neles = (nx + 1)*(ny + 1), nx*ny
-# x = np.linspace(0.0, 0.5, nx + 1)
-# y = np.linspace(0.0, 0.5, ny + 1)
-# X, Y = np.meshgrid(x, y)
-# nodes = zeros(nnodes,2)
-# nodes[:,1], nodes[:,2] = X'[:], Y'[:]
-# ndofs = 2
-
-# EBC, g = zeros(Int64, nnodes, ndofs), zeros(nnodes, ndofs)
-
-# EBC[collect(1:nx+1), :] .= -1
-
-# function ggt(t)
-#     v = 0.01
-#     if t<1.0
-#         state = t*v*ones(sum(EBC.==-2))
-#     elseif t<3.0
-#         state = (0.02 - t*v)*ones(sum(EBC.==-2))
-#     end
-#     return state, zeros(sum(EBC.==-2))
-# end
-# gt = ggt
-
-
-
-# #pull in the y direction
-# NBC, fext = zeros(Int64, nnodes, ndofs), zeros(nnodes, ndofs)
-# NBC[collect((nx+1)*ny + 1:(nx+1)*ny + nx+1), 2] .= -1
-# fext[collect((nx+1)*ny + 1:(nx+1)*ny + nx+1), 2] = [2.0,3.0]*1e8
-
-
-prop = Dict("name"=> testtype, "rho"=> 8000.0, "E"=> 200e+9, "nu"=> 0.45,
-"sigmaY"=>0.3e+9, "K"=>1/9*200e+9, "nn"=>post_nn)
-
+prop = Dict("name"=> testtype, "rho"=> 0.1, "E"=> 200, "B"=> 10.0,
+            "sigmaY"=>0.300, "K"=>1/9*200, "A0"=> 1.0, "eta"=> 10.0, "nn"=>post_nn)
 elements = []
-for j = 1:ny
-    for i = 1:nx 
-        n = (nx+1)*(j-1) + i
-        elnodes = [n, n + 1, n + 1 + (nx + 1), n + (nx + 1)]
-        coords = nodes[elnodes,:]
-        push!(elements,SmallStrainContinuum(coords,elnodes, prop,2))
-    end
+for i = 1:nx 
+    elnodes = [i, i+1]; coords = nodes[elnodes,:];
+    push!(elements, FiniteStrainTruss(coords,elnodes, prop, ngp))
 end
-
-
-domain = Domain(nodes, elements, ndofs, EBC, g, NBC, fext)
+domain = Domain(nodes, elements, ndofs, EBC, g, FBC, fext)
 state = zeros(domain.neqs)
 ∂u = zeros(domain.neqs)
-globdat = GlobalData(state,zeros(domain.neqs),
-                    zeros(domain.neqs),∂u, domain.neqs, gt)
-
+globdat = GlobalData(state,zeros(domain.neqs), zeros(domain.neqs),∂u, domain.neqs, gt, ft)
 assembleMassMatrix!(globdat, domain)
+# need to update state in domain from globdat
 updateStates!(domain, globdat)
+
 
 nntype = "nn"
 H_ = Variable(diagm(0=>ones(3)))
