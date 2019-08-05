1920bc0e4aafaf788d997fc8bc83aa1c00b3aae0
Zhengyu-Huang <hzyhzy2014@gmail.com>
2019-08-05 15:21:41 -0700

    add Truss2D

---
+++ test/Truss/Case2D/Data_NNTrussPull_Plasticity.jl
@@ -0,0 +1,51 @@
+using Revise
+using Test 
+using NNFEM
+using PyCall
+using PyPlot
+using JLD2
+using ADCME
+using LinearAlgebra
+
+testtype = "Plasticity1D" 
+
+prop = Dict("name"=> testtype, "rho"=> 0.1, "E"=> 200, "B"=> 10.0,
+            "sigmaY"=>0.300, "K"=>1/9*200, "A0"=> 1.0, "eta"=> 10.0)
+
+include("NNTrussPull_Domain.jl")
+
+
+
+
+
+
+domain = Domain(nodes, elements, ndofs, EBC, g, FBC, fext)
+state = zeros(domain.neqs)
+∂u = zeros(domain.neqs)
+globdat = GlobalData(state,zeros(domain.neqs), zeros(domain.neqs),∂u, domain.neqs, gt, ft)
+assembleMassMatrix!(globdat, domain)
+
+# need to update state in domain from globdat
+updateStates!(domain, globdat)
+
+
+T = 0.5
+NT = 20
+Δt = T/NT
+for i = 1:NT
+    
+    solver = NewmarkSolver(Δt, globdat, domain, -1.0, 0.0, 1e-5, 100)
+    
+    
+end
+
+# error()
+# todo write data
+write_data("$(@__DIR__)/Data/1.dat", domain)
+# plot
+close("all")
+scatter(nodes[:, 1], nodes[:,2], color="red")
+u,v = domain.state[1:domain.nnodes], domain.state[domain.nnodes+1:end]
+scatter(nodes[:, 1] + u, nodes[:,2] + v, color="blue")
+
+@save "Data/domain.jld2" domain
---
+++ test/Truss/Case2D/Train_NNTrussPull_Plasticity.jl
@@ -0,0 +1,82 @@
+#= 
+
+
+=#
+
+
+using Revise
+using Test 
+using NNFEM
+using PyCall
+using PyPlot
+using JLD2
+using ADCME
+using LinearAlgebra
+reset_default_graph()
+include("nnutil.jl")
+
+testtype = "NeuralNetwork1D"
+
+
+prop = Dict("name"=> testtype, "rho"=> 0.1, "E"=> 200.0, "B"=> 10.0,
+            "sigmaY"=>0.300, "K"=>1/9*200, "A0"=> 1.0, "eta"=> 10.0, "nn"=>post_nn)
+
+include("NNTrussPull_Domain.jl")
+
+
+
+
+# domain = Domain(nodes, elements, ndofs, EBC, g, FBC, fext)
+@load "Data/domain.jld2" domain
+state = zeros(domain.neqs)
+∂u = zeros(domain.neqs)
+globdat = GlobalData(state,zeros(domain.neqs), zeros(domain.neqs),∂u, domain.neqs, gt, ft)
+assembleMassMatrix!(globdat, domain)
+
+
+T = 0.5
+NT = 20
+Δt = T/NT
+
+
+nntype = "ae_scaled"
+
+
+E = prop["E"]
+H0 = zeros(1,1)
+H0[1,1] = E
+
+n_data = 1
+losses = Array{PyObject}(undef, n_data)
+for i = 1:n_data
+    state_history, fext_history = read_data("$(@__DIR__)/Data/$i.dat")
+    losses[i] = DynamicMatLawLoss(domain, globdat, state_history, fext_history, nn,Δt)
+end
+loss = sum(losses)
+
+variable_scope(nntype) do
+    global opt = AdamOptimizer().minimize(loss)
+end
+
+sess = Session(); init(sess)
+@show run(sess, loss)
+
+for i = 1:500
+    l, _ = run(sess, [loss, opt])
+    @show i,l
+    # if l<20000
+    #     break
+    # end
+end
+
+ADCME.save(sess, "Data/trained_nn_fem.mat")
+# BFGS!(sess, loss, 800)
+
+X, Y = prepare_strain_stress_data1D(domain)
+y = squeeze(nn(constant(X[:,1]),constant(X[:,2]),constant(X[:,3])))
+close("all")
+out = run(sess, y)
+plot(X[:,1], out,"+", label="NN")
+plot(X[:,1], Y, ".", label="Exact")
+legend()
+
