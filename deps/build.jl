e7d3e6fc5844affac44c5e2429d4495f8b4c053c
Kailai Xu <kailaix@stanford.edu>
2019-08-26 20:14:57 -0700

    update

---
+++ deps/build.jl
@@ -0,0 +1,16 @@
+function buildops(dirname)
+    PWD = pwd()
+    cd(joinpath("$(@__DIR__)/../", dirname))
+    if !isdir("build")
+        mkdir("build")
+    end
+    cd("build")
+    run(`cmake ..`)
+    run(`make -j`)
+    cd(PWD)
+end
+
+
+buildops("test/Plate/MultiScale/Ops/Sym")
+buildops("test/Plate/Hyperelasticity/Ops/Sym")
+buildops("test/Plate/Plasticity/Ops/Sym")
