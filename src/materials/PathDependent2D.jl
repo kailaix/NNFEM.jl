cc5d212b55507d85c082b52bc90d08f8068caa20
Daniel_Zhengyu_Huang <hzyhzy2014@gmail.com>
2019-09-13 17:00:14 -0700

    start PathDependent2D

--- src/materials/PathDependent2D.jl
+++ src/materials/PathDependent2D.jl
@@ -5,15 +5,36 @@ mutable struct PathDependent2D
     E::Float64 # Young's modulus
     ν::Float64 # Poisson's ratio
     ρ::Float64 # density
-    # hardening parameter, yield function = f - (σY + Kα)
-    K::Float64 
-    σY::Float64
-    α::Float64 
-    α_::Float64 # α to be updated in `commitHistory`
     σ0::Array{Float64} # stress at last time step
     σ0_::Array{Float64} # σ0 to be updated in `commitHistory`
     ε0::Array{Float64} # stress at last time step
     ε0_::Array{Float64} # σ0 to be updated in `commitHistory`
+end
+
+
+@doc """
+    The power-law hardening law has the the following format
+    σY = (0.1 + 0.3 ε_equiv^0.4)MPa
+
+""" -> 
+function f_σY(σ, σY, K)
+    return sqrt(σ[1]^2-σ[1]*σ[2]+σ[2]^2+3*σ[3]^2)-σY-K*α
+end
+
+function fσ(σ)
+    σ1, σ2, σ3 = σ[1], σ[2], σ[3]
+    J2 = sqrt(σ1^2-σ1*σ2+σ2^2+3*σ3^2)
+    z = [(σ1 - σ2/2)/J2;
+        (-σ1/2 + σ2)/J2;
+        3*σ3/J2]
+end
+
+function fσσ(σ)
+    σ1, σ2, σ3 = σ[1], σ[2], σ[3]
+    J2 = sqrt(σ1^2-σ1*σ2+σ2^2+3*σ3^2)
+    [     (-σ1 + σ2/2)*(σ1 - σ2/2)/J2^3 + 1/J2 (σ1/2 - σ2)*(σ1 - σ2/2)/J2^3 - 1/(2*J2)                                   -3*σ3*(σ1 - σ2/2)/J2^3;
+    (-σ1 + σ2/2)*(-σ1/2 + σ2)/J2^3 - 1/(2*J2)    (-σ1/2 + σ2)*(σ1/2 - σ2)/J2^3 + 1/J2                                  -3*σ3*(-σ1/2 + σ2)/J2^3;
+    3*σ3*(-σ1 + σ2/2)/J2^3                                                        3*σ3*(σ1/2 - σ2)/J2^3 -9*σ3^2/J2^3 + 3/J2]
 end
 
 
@@ -46,16 +67,27 @@ function getStress(self::PathDependent2D,  strain::Array{Float64},  Dstrain::Arr
     σ0 = self.σ0 
     
     E = self.E
+    H = self.H
     
-    α = 100
-    σ = σ0 + E*(ε-ε0) #* (1.0 + α*ε[1] + ε[2] + ε[3])
-    dΔσdΔε  =  [E 0 0 ; 0 E 0; 0 0 E] #* (1.0 + α*ε[1] + ε[2] + ε[3]) + [α*H*(ε-ε0) H*(ε-ε0) H*(ε-ε0)]
-
-    #@show ε[1] , ε[2] , ε[3]
-
-    self.σ0_ = σ
+    σ = σ0 + H*(ε-ε0) 
+    σY = f_σY(ε)
+    #@show r2, α, K, σY
+    if σ < σY
+        #@show "Elasticity"
+        σ = σ0 + H*(ε-ε0) 
+        dΔσdΔε = H
+        #@show "elastic", α, α0, σ, σ0,ε, ε0, σ0 + H*(ε-ε0) , H*ε
+    else
+        # error()
+        σ = σY
+        dΔσdΔε = df_σY(ε)
+        
+    end
+    # #@show Δγ
+    self.σ0_ = σ[:]
     self.ε0_ = ε
-
+    # self.σ0_ = self.σ0
+    # #@show σ, dΔσdΔε
     return σ, dΔσdΔε
 end
 
