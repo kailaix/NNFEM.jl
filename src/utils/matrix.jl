04e463501475352093b6afab13f23c13dfc0fb77
Kailai Xu <kailaix@hotmail.com>
2019-12-09 18:30:44 -0800

    update doc

--- src/utils/matrix.jl
+++ src/utils/matrix.jl
@@ -3,14 +3,15 @@ export sym_H, orthotropic_H, spd_H, spd_Cholesky, spd_Chol_Orth, spd_zero_to_H
 
 @doc raw"""
     sym_H(y::PyObject)
+    sym_H(o::Array)
 
 Creates a symmetric matrix from 6 parameters
 ```math
-H = \begin{matrix}
+H = \begin{bmatrix}
 y_1 & y_2 & y_3\\ 
 y_2 & y_4 & y_5 \\ 
 y_3 & y_5 & y_6
-\end{matrix}
+\end{bmatrix}
 ```
 """
 function sym_H(y::PyObject)
@@ -27,14 +28,15 @@ end
 
 @doc raw"""
     orthotropic_H(y::PyObject)
+    orthotropic_H(o::Array)
 
 Creates a symmetric matrix from 4 parameters
 ```math
-H = \begin{matrix}
+H = \begin{bmatrix}
 y_1 & y_2 & 0\\ 
 y_2 & y_3 & 0 \\ 
 0 & 0 & y_4
-\end{matrix}
+\end{bmatrix}
 ```
 """
 function orthotropic_H(y::PyObject)
@@ -81,6 +83,7 @@ end
 
 @doc raw"""
     spd_Cholesky(o::Array)
+    spd_Cholesky(o::PyObject)
 
 Creates a SPD matrix from 6 scalars. 
 
@@ -89,11 +92,11 @@ A = LL'
 ```
 where
 ```math
-L = \begin{matrix}
-o_1 & & 
-o_2 & o_4 &
+L = \begin{bmatrix}
+o_1 & &  \\
+o_2 & o_4 & \\
 o_3 & o_5 & o_6 
-\end{matrix}
+\end{bmatrix}
 ```
 """
 function spd_Cholesky(o::Array)
@@ -103,11 +106,7 @@ function spd_Cholesky(o::Array)
      o[1]*o[3] o[2]*o[3]+o[4]*o[5] o[3]*o[3]+o[5]*o[5]+o[6]*o[6]]
 end
 
-"""
-    spd_Cholesky(o::PyObject)
 
-Creates a SPD matrix from 6 scalars. `o` is a ``n\\times6`` tensor.
-"""
 function spd_Cholesky(o::PyObject)
     if size(o,2)!=6
         error("NNFEM: second dimension of `o` must be 6")
@@ -121,6 +120,7 @@ end
 
 @doc raw"""
     spd_Chol_Orth(o::Array)
+    spd_Chol_Orth(o::PyObject)
 
 Creates a SPD matrix from 4 scalars. 
 
@@ -129,11 +129,11 @@ A = LL'
 ```
 where
 ```math
-L = \begin{matrix}
-o_1 & & 
-o_2 & o_3 &
- &  & o_4
-\end{matrix}
+L = \begin{bmatrix}
+o_1 & & \\
+o_2 & o_3 & \\
+ &  & o_4 
+\end{bmatrix}
 ```
 """
 function spd_Chol_Orth(o::Array)
@@ -143,11 +143,6 @@ function spd_Chol_Orth(o::Array)
      0.0       0.0                 o[4]*o[4]]
 end
 
-"""
-    spd_Chol_Orth(o::PyObject)
-
-Creates a SPD matrix from 6 scalars. `o` is a ``n\\times6`` tensor.
-"""
 function spd_Chol_Orth(o::PyObject)
     if size(o,2)!=4
         error("NNFEM: second dimension of `o` must be 4")
@@ -160,6 +155,7 @@ end
 
 @doc raw"""
     spd_zero_to_H(o::Array)
+    spd_zero_to_H(o::Array, H0inv::Array{Float64,2})
 
 Creates a SPD matrix from 4 scalars. 
 
@@ -168,11 +164,11 @@ A = (H_0^{-1} +LL')^{-1}
 ```
 where
 ```math
-L =  \begin{matrix}
-o_1 & & 
-o_2 & o_3 &
+L =  \begin{bmatrix}
+o_1 & & \\
+o_2 & o_3 & \\
  &  & o_4
-\end{matrix}
+\end{bmatrix}
 ```
 
 """
