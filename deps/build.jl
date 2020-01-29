5287429cac5f9ed4288d89d3c9581b76decd1ba2
Kailai Xu <kailaix@stanford.edu>
2020-01-29 14:22:47 -0800

    update SPDOp

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,7 @@
 using ADCME
 function buildops(dirname)
     PWD = pwd()
-    cd(joinpath("$(@__DIR__)/../", dirname))
+    cd(joinpath("$(@__DIR__)/CustomOp/", dirname))
     if !isdir("build")
         mkdir("build")
         cd("build")
@@ -11,12 +11,12 @@ function buildops(dirname)
     cd(PWD)
 end
 
-buildops("deps/CustomOp/FintComp")
-buildops("deps/CustomOp/SymOp")
-buildops("deps/CustomOp/OrthotropicOp")
-buildops("deps/CustomOp/SPDOp")
-buildops("deps/CustomOp/CholOp")
-buildops("deps/CustomOp/CholOrthOp")
+buildops("FintComp")
+buildops("SymOp")
+buildops("OrthotropicOp")
+buildops("SPDOp")
+buildops("CholOp")
+buildops("CholOrthOp")
 
 # # build ADLaw
 # PWD = pwd()
