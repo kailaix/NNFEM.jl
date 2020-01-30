86fc69cefd1da809052205d765b87cae21ed19bc
Daniel Huang <zhengyuh@stanford.edu>
2020-01-30 09:35:21 -0800

    Plasticity-Small

---
+++ test/Plate/Plasticity-Small/GenerateData.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+force_scale=50.0
+
+julia Data_NNPlatePull.jl 100 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 101 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 102 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 103 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 104 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 105 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 106 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 200 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 201 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 202 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 203 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 204 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 205 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 206 $force_scale 2 2 &
+julia Data_NNPlatePull.jl 300 $force_scale 2 2 &
+
+
+
+wait
---
+++ test/Plate/Plasticity-Small/NNTrain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+# Execute jobs in parallel
+#julia NN_Train_NNPlatePull.jl 0 spd_Chol_Orth 0 &
+#julia NN_Train_NNPlatePull.jl 1 spd_Chol_Orth 0 &
+
+
+
+
+julia NN_Train_NNPlatePull.jl  1 spd_Chol_Orth 0 &
+julia NN_Train_NNPlatePull.jl  2 spd_Chol_Orth 0 &
+
+#julia NNPreLSfit.jl 0 spd_Chol_Orth 0 &
+#julia NNPreLSfit.jl 1 spd_Chol_Orth 0 &
+
+
+
+# Execute jobs in parallel
+#julia NN_Train_NNPlatePull.jl 0 spd_Chol_Orth 0 &
+#julia NN_Train_NNPlatePull.jl 1 spd_Chol_Orth 0 &
+
+wait
