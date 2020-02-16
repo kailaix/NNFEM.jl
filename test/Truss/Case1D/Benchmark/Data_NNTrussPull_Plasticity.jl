900a303b1a5bc3ba6188bb1ec22b33372dd658af
Kailai Xu <kailaix@hotmail.com>
2020-02-16 15:26:50 -0800

    add benchmark

---
+++ test/Truss/Case1D/Benchmark/Data_NNTrussPull_Plasticity.jl
@@ -0,0 +1,57 @@
+using Revise
+using Test 
+using NNFEM
+using PyCall
+using PyPlot
+using JLD2
+using ADCME
+using LinearAlgebra
+
+
+
+include("NNTrussPull_Domain.jl")
+testtype = "Plasticity1D" 
+
+tid = length(ARGS)>=1 ? parse(Int64, ARGS[1]) : 3
+
+# kg, m,  ms
+prop = Dict("name"=> testtype, "rho"=> 8000.0, "E"=> 200e3, "nu"=> 0.45,
+           "sigmaY"=>0.3e3, "K"=>1/9*200e3, "B"=> 0.0, "A0"=> 0.005)
+
+
+           
+
+nodes, EBC, g, gt, FBC, fext, ft, npoints, node_to_point = BoundaryCondition(tid)
+elements = []
+for i = 1:nx 
+    elnodes = [i, i+1]; coords = nodes[elnodes,:];
+    push!(elements, FiniteStrainTruss(coords,elnodes, prop, ngp))
+end
+domain = Domain(nodes, elements, ndofs, EBC, g, FBC, fext)
+state = zeros(domain.neqs)
+∂u = zeros(domain.neqs)
+globdat = GlobalData(state,zeros(domain.neqs), zeros(domain.neqs),∂u, domain.neqs, gt, ft)
+assembleMassMatrix!(globdat, domain)
+# need to update state in domain from globdat
+updateStates!(domain, globdat)
+
+
+
+for i = 1:NT
+    solver = NewmarkSolver(Δt, globdat, domain, -1.0, 0.0, 1e-8, 1e-8, 100) # ok
+    printstyled("============================== iteration $i ==============================\n",color=:green)
+end
+
+if !isdir("$(@__DIR__)/Data/")
+    mkdir("$(@__DIR__)/Data/")
+end
+
+write_data("$(@__DIR__)/Data/$tid.dat", domain)
+@save "$(@__DIR__)/Data/domain$tid.jld2" domain
+# plot
+# close("all")
+# scatter(nodes[:, 1], nodes[:,2], color="red")
+# u,v = domain.state[1:domain.nnodes], domain.state[domain.nnodes+1:end]
+# scatter(nodes[:, 1] + u, nodes[:,2] + v, color="blue")
+
+# @save "Data/domain.jld2" domain
---
+++ test/Truss/Case1D/Benchmark/NNLearn.jl
@@ -0,0 +1,41 @@
+using Revise
+using ADCME
+using NNFEM
+using JLD2
+using PyPlot
+using MAT 
+using DelimitedFiles
+reset_default_graph()
+
+include("nnutil.jl")
+
+loss = constant(0.0)
+for tid = [1,2,4,5]
+    strain, stress = read_strain_stress("Data/$(tid).dat")
+    X, Y = prepare_strain_stress_data1D(strain, stress )
+    y = squeeze(nn(constant(X[:,1:1]), constant(X[:,2:2]), constant(X[:,3:3])))
+    global loss += sum((y - Y)^2)
+end
+sess = Session(); init(sess)
+@show run(sess,loss)
+
+loss_all = []
+for i = 1:1
+    println("************************** Outer Iteration = $i ************************** ")
+    loss_ = BFGS!(sess, loss, 3000)
+    push!(loss_all, loss_)
+end
+# ADCME.save(sess, "Data/$FILEID.mat")
+# loss_ = vcat(loss_all...)
+# writedlm("Data/$FILEID.txt", loss_)
+
+
+strain, stress = read_strain_stress("Data/3.dat")
+X, Y = prepare_strain_stress_data1D(strain, stress )
+y = squeeze(nn(constant(X[:,1:1]), constant(X[:,2:2]), constant(X[:,3:3])))
+testerror = sqrt(mean((y-Y)^2))
+
+testerror_ = run(sess, testerror)
+open("Data/$FILEID2.txt", "a") do io 
+    writedlm(io, [testerror_])
+end
