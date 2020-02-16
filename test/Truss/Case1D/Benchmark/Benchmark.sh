58c8689e08c493cdbcc81e461ffd4dc0a2716a47
Kailai Xu <kailaix@hotmail.com>
2020-02-16 15:47:51 -0800

    update benchmark

--- test/Truss/Case1D/Benchmark/Benchmark.sh
+++ test/Truss/Case1D/Benchmark/Benchmark.sh
@@ -1,10 +1,33 @@
-for nn_depth in 5 10 20 40 80
+# thresholding 
+for nn_depth in 3 
 do 
-for nn_width in 3 10 20
+for nn_width in 20
 do 
-for sigmaY in 0.01e6 0.1e6 1e6 10e6
+for sigmaY in 0.001e6 0.01e6 0.1e6 1e6
 do 
 for dsigma in 0.1 1.0 10.0 100.0
+do 
+for activation in tanh
+do 
+for exp_id in 1 2 3 4 5
+do 
+srun -n 1 -N 1 julia NNLearn $nn_depth $nn_width $sigmaY $dsigma $activation $exp_id &
+done
+done 
+done
+done 
+done
+done  
+
+
+# nn architecture 
+for nn_depth in 3 5 10 20 
+do 
+for nn_width in 5 20 40 
+do 
+for sigmaY in 0.01e6
+do 
+for dsigma in 1.0
 do 
 for activation in tanh relu leaky_relu selu elu 
 do 
@@ -17,3 +40,4 @@ done
 done 
 done
 done  
+
