137edf689b0ca42ae31769e86977f1e777ebf7dc
Kailai Xu <kailaix@stanford.edu>
2020-02-17 09:56:16 -0800

    update

--- test/Truss/Case1D/Benchmark/NNLearn.jl
+++ test/Truss/Case1D/Benchmark/NNLearn.jl
@@ -24,18 +24,43 @@ for i = 1:10
     println("************************** Outer Iteration = $i ************************** ")
     loss_ = BFGS!(sess, loss, 3000)
     push!(loss_all, loss_)
+end 
+
+testtype = "NeuralNetwork1D"
+include("NNTrussPull_Domain.jl")
+tid = 3
+
+nodes, EBC, g, gt, FBC, fext, ft, npoints, node_to_point = BoundaryCondition(tid)
+prop = Dict("name"=> testtype, "rho"=> 8000.0, "E"=> 200e3, "nu"=> 0.45,
+           "sigmaY"=>0.3e3, "K"=>1/9*200e3, "B"=> 0.0, "A0"=> 0.005, "nn"=>post_nn)
+
+elements = []
+for i = 1:nx 
+    elnodes = [i, i+1]; coords = nodes[elnodes,:];
+    push!(elements, FiniteStrainTruss(coords,elnodes, prop, ngp))
 end
-# ADCME.save(sess, "Data/$FILEID.mat")
-# loss_ = vcat(loss_all...)
-# writedlm("Data/$FILEID.txt", loss_)
+domain = Domain(nodes, elements, ndofs, EBC, g, FBC, fext)
+state = zeros(domain.neqs)
+∂u = zeros(domain.neqs)
+globdat = GlobalData(state,zeros(domain.neqs), zeros(domain.neqs),∂u, domain.neqs, gt, ft)
+assembleMassMatrix!(globdat, domain)
+# need to update state in domain from globdat
+updateStates!(domain, globdat)
 
 
-strain, stress = read_strain_stress("Data/3.dat")
-X, Y = prepare_strain_stress_data1D(strain, stress )
-y = squeeze(nn(constant(X[:,1:1]), constant(X[:,2:2]), constant(X[:,3:3])))
-testerror = sqrt(mean((y-Y)^2))
+adaptive_solver_args = Dict("Newmark_rho"=> 0.0, 
+                          "Newton_maxiter"=>10, 
+                          "Newton_Abs_Err"=>1e-4, 
+                          "Newton_Rel_Err"=>1e-6, 
+                          "damped_Newton_eta" => 1.0)
+globdat, domain, ts = AdaptiveSolver("NewmarkSolver", globdat, domain, T, NT, adaptive_solver_args)
 
-testerror_ = run(sess, testerror)
-open("Data/$FILEID2.txt", "a") do io 
-    writedlm(io, [testerror_])
+state_ = hcat(domain.history["state"]...)
+
+
+@load "Data/domain3.jld2" domain 
+state = hcat(domain.history["state"]...)
+loss = norm(state_ - state)
+open("Data/$FILEID2.txt", "a") do io
+    writedlm(io, [loss])
 end
