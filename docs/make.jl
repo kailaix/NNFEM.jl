478313cc055834df711b156bc29a1e8ca924c6ba
Kailai Xu <kailaix@stanford.edu>
2020-04-01 00:16:58 -0700

    update doc

--- docs/make.jl
+++ docs/make.jl
@@ -7,10 +7,6 @@ using NNFEM
 makedocs(sitename="NNFEM", modules=[NNFEM],
 pages = Any[
     "index.md",
-    "api.md",
-    "method.md",
-    "plasticity.md",
-    "instructions.md",
     "api.md"
 ],
 authors = "Kailai Xu and Daniel (Zhengyu) Huang")
