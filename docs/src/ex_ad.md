651302a0e1d212f93acedd83630639ab34e00b53
Kailai Xu <kailaix@stanford.edu>
2020-04-10 18:37:07 -0700

    update

--- docs/src/ex_ad.md
+++ docs/src/ex_ad.md
@@ -3,9 +3,73 @@
 ```julia
 using NNFEM
 
+
 domain = example_domain()
+globaldata = example_global_data(domain)
 init_nnfem(domain)  # IMPORTANT: initialize the NNFEM session
 
-K = s_compute_fem_stiffness_matrix()
+# total number of gauss points
+ngauss = length(domain.elements[1].weights) * domain.neles
+H = constant(rand(ngauss, 3, 3)) # linear elasticity matrix 
+K = s_compute_stiffness_matrix(H, domain) # stiffness matrix
+assembleMassMatrix!(globaldata, domain)
+M = SparseTensor(globaldata.M) # mass matrix
+
+a = 0.1
+b = 0.2
+A = a * K + b * M
+
+rhs = constant(rand(size(K,2)))
+sol = A\rhs
 ```
+
+1. `sol` can be differentiated with respect to `rhs`
+
+```
+julia> gradients(sum(sol), rhs)
+PyObject <tf.Tensor 'gradients_1/IdentityN_12_grad/SparseSolverGrad:3' shape=(242,) dtype=float64>
+```
+
+2. `sol` can be differentiated with respect to `H`
+
+```
+julia> gradients(sum(sol), H)
+PyObject <tf.Tensor 'gradients_2/IdentityN_11_grad/SmallContinuumStiffnessGrad:0' shape=(400, 3, 3) dtype=float64>
+```
+
+---
+
+
+
+Given the stress `stress`, we can compute the internal force and evaluate its gradients
+
+```julia
+stress = constant(rand(ngauss, 3))
+fint = s_compute_internal_force_term(stress, domain)
+gradients(sum(fint), stress)
+```
+
+Expected
+
+```
+PyObject <tf.Tensor 'gradients_3/IdentityN_13_grad/SmallContinuumFintGrad:0' shape=(400, 3) dtype=float64>
+```
+
+---
+
+Given the displacement, we can evaluate the strain and evaluate the gradients
+
+```julia
+state = constant(rand(domain.nnodes*2))
+strain = s_eval_strain_on_gauss_points(state, domain)
+gradients(sum(strain), state)
+```
+
+Expected
+
+```
+PyObject <tf.Tensor 'gradients_4/IdentityN_14_grad/SmallContinuumStrainGrad:0' shape=(242,) dtype=float64>
+```
+
+
 
--- src/AD/kernels.jl
+++ src/AD/kernels.jl
@@ -1,7 +1,7 @@
 
 export init_nnfem, example_domain, 
 s_eval_strain_on_gauss_points, s_compute_stiffness_matrix,
-s_compute_internal_force_term
+s_compute_internal_force_term, example_global_data
 """
     init_nnfem(domain::Domain)
 
@@ -65,19 +65,29 @@ function example_domain(m::Int64 = 10, n::Int64 = 10, h::Float64 = 1.0)
 
     for i = 2:m
         idx = n*(m+1)+i
-        FBC[idx,:] .= -2
-        FBC[i,:] .= -2
+        FBC[idx,:] .= -1
+        FBC[i,:] .= -1
     end
 
     for j = 1:n+1 
         idx = (j-1)*(m+1)+1
-        FBC[idx,:] .= -2
+        FBC[idx,:] .= -1
         idx = (j-1)*(m+1)+m+1
-        FBC[idx,:] .= -2
+        FBC[idx,:] .= -1
     end
 
     ndims = 2
     domain = Domain(coords, elements, ndims, EBC, g, FBC, f)
+end
+
+function example_global_data(domain::Domain)
+    Dstate = zeros(domain.neqs)
+    state = zeros(domain.neqs)
+    velo = zeros(domain.neqs)
+    acce = zeros(domain.neqs)
+    gt = nothing
+    ft = nothing
+    globdat = GlobalData(state, Dstate, velo, acce, domain.neqs, gt, ft)
 end
 
 
@@ -87,7 +97,7 @@ end
 Computes the small strain stiffness matrix. $k$ is a $n\times 3\times 3$ matrix, where $n$ is the total number of Gauss points.
 Returns a SparseTensor. 
 """
-function s_compute_stiffness_matrix(k::Union{Array{Float64,3}, PyObject})
+function s_compute_stiffness_matrix(k::Union{Array{Float64,3}, PyObject}, domain::Domain)
     small_continuum_stiffness_ = load_op_and_grad("$(@__DIR__)/../../deps/CustomOp/SmallContinuumStiffness/build/libSmallContinuumStiffness","small_continuum_stiffness", multiple=true)
     k = convert_to_tensor([k], [Float64]); k = k[1]
     ii, jj, vv = small_continuum_stiffness_(k)
@@ -100,7 +110,7 @@ end
 
 Computes the strain on Gauss points in the small strain case. `state` is the full displacement vector. 
 """
-function s_eval_strain_on_gauss_points(state::Union{Array{Float64,1}, PyObject})
+function s_eval_strain_on_gauss_points(state::Union{Array{Float64,1}, PyObject}, domain::Domain)
     small_continuum_strain_ = load_op_and_grad("$(@__DIR__)/../../deps/CustomOp/SmallContinuumStrain/build/libSmallContinuumStrain","small_continuum_strain")
     state = convert_to_tensor([state], [Float64]); state = state[1]
     ep = small_continuum_strain_(state)
@@ -115,7 +125,7 @@ Computes the internal force
 $$\int_\Omega \sigma : \delta \epsilon dx$$
 Only active DOFs are considered. 
 """
-function s_compute_internal_force_term(stress::Union{Array{Float64,2}, PyObject})
+function s_compute_internal_force_term(stress::Union{Array{Float64,2}, PyObject}, domain::Domain)
     small_continuum_fint_ = load_op_and_grad("./build/libSmallContinuumFint","small_continuum_fint")
     stress = convert_to_tensor([stress], [Float64]); stress = stress[1]
     out = small_continuum_fint_(stress)
