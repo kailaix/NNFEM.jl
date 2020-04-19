4b80056f7eef18841684b9acf132ca03f3a8dfce
Kailai Xu <kailaix@stanford.edu>
2020-04-19 11:48:00 -0700

    update

--- docs/src/codes/hyperelasticity_nn.jl
+++ docs/src/codes/hyperelasticity_nn.jl
@@ -1,5 +1,5 @@
 include("hyperelasticity.jl")
-
+# 
 ts = ExplicitSolverTime(Δt, NT)
 ubd, abd = compute_boundary_info(domain, globaldata, ts)
 Fext = compute_external_force(domain, globaldata, ts) 
@@ -8,28 +8,56 @@ d0 = zeros(2domain.nnodes)
 v0 = zeros(2domain.nnodes)
 a0 = zeros(2domain.nnodes)
 
-# general neural network 
-function nn_law(ε)
-  ae(ε, [20,20,20,3])
+
+mode = "linear"
+if length(ARGS)>=1
+  global mode = ARGS[1]
 end
 
-# free energy 
-function nn_law(ε)
-  φ = squeeze(ae(ε, [20,20,20,1]))
-  tf.gradients(φ, ε)
-end
+if mode=="linear"
+  global Dc, nn_law
+  Dc = Variable(rand(3,3))
+  Dc = spd(Dc)
+  function nn_law(strain)
+    strain_tensor = strain_voigt_to_tensor(strain)
+    stress = strain*Dc
+    stress
+  end
 
-# parameter
-c1 = Variable(0.5) * 1e-1
-c2 = Variable(0.5) * 1e-1
-function nn_law(ε)
-  compute_stress_rivlin_saunders(ε, c1, c2)
-end
+elseif mode=="consistent_tangent"
+  global Dc, nn_law
+  Dc = Variable(rand(3,3))
+  Dc = spd(Dc)
+  function nn_law(strain)
+    coef = ae(strain, [20,20,20,6])
+    coef = [coef constant(ones(size(coef,1), 1))]
+    H = consistent_tangent_matrix(coef, Dc)
+    stress = batch_matmul(H, strain)
+    stress
+  end
 
+elseif mode=="nn"
+  global Dc, nn_law
+  # general neural network 
+  function nn_law(ε)
+    ae(ε, [20,20,20,3])
+  end
+elseif mode=="free_energy"
+  global Dc, nn_law
+  # free energy 
+  function nn_law(ε)
+    φ = squeeze(ae(ε, [20,20,20,1]))
+    tf.gradients(φ, ε)[1]
+  end
+else 
+  error("$mode not valid")
+end
 
 d, v, a= ExplicitSolver(globaldata, domain, d0, v0, a0, Δt, NT, nn_law, Fext, ubd, abd; strain_type="finite")
 
-
+# test  = open("test.txt", "w")
+sess = Session(); init(sess)
+# run(sess, d)
 idx = @. (0:n)*m + 1
 idx = [idx; (@. idx + domain.nnodes)]
 loss = sum((d[:, idx] - d_[:, idx])^2)
@@ -37,20 +65,24 @@ sess = Session(); init(sess)
 @info run(sess, loss)
 # error()
 
-# BFGS!(sess, loss)
+!isdir(mode) && mkdir(mode)
 
+for i = 1:100
+  loss_ = BFGS!(sess, loss, 1000)
+  d0 = run(sess, d)
 
-# for visualizing the parameters 
-sol = []
-cb = (vs, iter, loss)->begin 
-  push!(sol, vs)
-  printstyled("[#iter $iter] a = $vs, loss=$loss\n", color=:green)
+  # visualize
+  p = visualize_displacement(d0, domain)
+  saveanim(p, "$mode/$i.gif")
+  close("all")
+  plot(d_[:,1], "-", color="C1")
+  plot(d0[:,1], "--", color="C1")
+  plot(d_[:,1+domain.nnodes], color="C2")
+  plot(d0[:,1+domain.nnodes], "--", color="C2")
+  savefig(p, "$mode/$i.png")
+
+  open("$mode/loss.txt", "a") do io 
+    writedlm(io, loss_)
+  end  
+  ADCME.save(sess, "$mode/$i.mat")
 end
-BFGS!(sess, loss, callback = cb, vars = [c1, c2])
-
-plot([x[1] for x in sol], "+--", color="red", label="\$C1\$")
-plot(ones(length(sol))*1e-1, "-", color="k", alpha=0.3, label="Reference Value (C1 and C2)")
-plot([x[2] for x in sol], "x--", color ="green", label="\$C2\$")
-xlabel("Iterations")
-legend()
-
