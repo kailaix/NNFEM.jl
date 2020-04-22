80b0a464ab187d3c64e99a01cad0ece6726216db
Kailai Xu <kailaix@stanford.edu>
2020-04-22 10:42:03 -0700

    add scaled

--- src/utils/Visualize2.jl
+++ src/utils/Visualize2.jl
@@ -87,6 +87,7 @@ function visualize_displacement(u::Array{Float64, 2}, domain::Domain)
     ylim(ymin, ymax)
     function update(frame)
         p.set_data(U0[:,frame], V0[:,frame])
+        axis("scaled")
         t.set_text("Snapshot = $frame")
     end
     out = animate(update, Int64.(round.(LinRange(1, NT, 20))))
